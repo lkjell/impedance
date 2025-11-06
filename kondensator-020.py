64a7a585e9d94898987c31a2e036d4981dd623f9
Kjell Le <lkjell@gmail.com>
2025-11-06 20:03:17 +0100

    Improve visibility

--- kondensator-020.py
+++ kondensator-020.py
@@ -5,36 +5,45 @@ import matplotlib.pyplot as plt
 
 # CIVIL
 
+#### KAN ENDRE Variabel ###########
 f = 1  # frekvens [Hz]
 U0 = 1  # [V] maks spenning
-C = 1 / (2 * np.pi * f)
+C = 1 / (2 * np.pi * f)  # Kapasitans, farad [F]
+tid = 3  # [s]
+###################################
 
-t = np.linspace(0, 3, 1000)  # s
+t = np.linspace(0, tid, 1000)  # s
 U = U0 * np.sin(2 * np.pi * f * t)
 
 # Derivere med respekt til tiden
 # Q = C*U => dQ/dt = i (strøm)
 I = C * U0 * np.cos(2 * np.pi * f * t) * 2 * np.pi * f
 I = C * U0 * np.sin(2 * np.pi * f * t + np.pi / 2) * 2 * np.pi * f
+I0 = C * U0 * 2 * np.pi * f  # [A] maks strøm
+
+P = U * I
 
 fig, axs = plt.subplots(2, 1)
 
 axs[0].plot(t, U)
 ax = axs[0].twinx()
 ax.plot(t, I, color="r")
-# ax.tick_params(axis="y")
 ax.set_ylabel("Strøm [A]", color="r")
 
 axs[0].set_xlabel("Tid [s]")
 axs[0].set_ylabel("Spenning [V]")
 axs[0].set_title("$U$")
+axs[0].grid(True)
 
-
-axs[1].plot(t, I, color="r")
+axs[1].plot(t, P, color="g")
 axs[1].set_xlabel("Tid [s]")
-axs[1].set_ylabel("Strøm [A]")
-axs[1].set_title("$I$")
+axs[1].set_ylabel("Effekt [W]", color="g")
+axs[1].set_title("$P$")
+axs[1].grid(True)
 
-fig.suptitle(f"AC spenning og strøm. Kondensator.")
+fig.suptitle(
+    f"AC kondensator: $f = {f}$Hz, $U_0 = {U0}$V, "
+    f"$I_0 = {I0:.3}$A, $C = {C:.3}$F, $\\theta = 90\\degree$"
+)
 plt.tight_layout()
 plt.show()
--- kondensator-021.py
+++ kondensator-021.py
@@ -5,17 +5,23 @@ import matplotlib.pyplot as plt
 
 # CIVIL
 
+#### KAN ENDRE Variabel ###########
 f = 1  # frekvens [Hz]
 U0 = 1  # [V] maks spenning
-L = 2 * np.pi * f  # Henry
+L = 1 / (2 * np.pi * f)  # Induktans, henry [H]
+tid = 3  # [s]
+###################################
 
-t = np.linspace(0, 3, 1000)  # s
+t = np.linspace(0, tid, 1000)  # s
 U = U0 * np.sin(2 * np.pi * f * t)
 
 # Integrerer med respekt til tiden
 # U_L =  Ldi/dt => di = (U_L / L) dt
-I = -np.cos(2 * np.pi * t) * (U0 / (2 * np.pi * f * L))
-I2 = np.sin(2 * np.pi * t - np.pi / 2) * (U0 / (2 * np.pi * f * L))
+I = -np.cos(2 * np.pi * f * t) * (U0 / (2 * np.pi * f * L))
+I = np.sin(2 * np.pi * f * t - np.pi / 2) * (U0 / (2 * np.pi * f * L))
+I0 = U0 / (2 * np.pi * f * L)  # [A] maks strøm
+
+P = U * I
 
 fig, axs = plt.subplots(2, 1)
 
@@ -27,15 +33,18 @@ ax.set_ylabel("Strøm [A]", color="r")
 
 axs[0].set_xlabel("Tid [s]")
 axs[0].set_ylabel("Spenning [V]")
-axs[0].set_title("$U$")
+axs[0].set_title("$U$ og $I$")
+axs[0].grid(True)
 
-
-axs[1].plot(t, I, color="r")
-axs[1].plot(t, I, color="r")
+axs[1].plot(t, P, color="g")
 axs[1].set_xlabel("Tid [s]")
-axs[1].set_ylabel("Strøm [A]")
-axs[1].set_title("$I$")
+axs[1].set_ylabel("Effekt [W]", color="g")
+axs[1].set_title("$P$")
+axs[1].grid(True)
 
-fig.suptitle(f"AC spenning og strøm. Spole.")
+fig.suptitle(
+    f"AC Spole: $f = {f}$Hz, $U_0 = {U0}$V, "
+    f"$I_0 = {I0:.3}$A, $L = {L:.3}$F, $\\theta = -90\\degree$"
+)
 plt.tight_layout()
 plt.show()
